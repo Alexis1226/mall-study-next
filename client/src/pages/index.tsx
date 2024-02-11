78e016dec8a3554a18dc9a98f486c5e83eb631cc
Alexis1226 <feuri41@gmail.com>
2024-02-11 13:19:08 +0900

    Next.js Setup

--- client/src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import '../scss/index.scss';
-
-const MainPage = () => <div>메인</div>;
-
-export default MainPage;
--- client/src/utill/arrToObj.ts
+++
@@ -1,7 +0,0 @@
-const arrToObj = (arr: [string, any][]) =>
-  arr.reduce<{ [key: string]: any }>((res, [key, val]) => {
-    res[key] = val;
-    return res;
-  }, {});
-
-export default arrToObj;
